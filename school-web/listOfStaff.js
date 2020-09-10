bf9e66a1d397ac00b6a60fffad7ce9fdae5823fb
DippreyB <48030605+DippreyB@users.noreply.github.com>
2020-09-10 16:07:09 -0500

    wrote function to make videos

---
+++ school-web/listOfStaff.js
@@ -0,0 +1,66 @@
+
+var staff = {
+    "boughton": {
+        "pictureUrl": "BoughtonHeadShot2020.jpg",
+        "video": "c3Ys663pTnI",
+        "name": "Amy Boughton",
+        "title": "Principal",
+        "phone": "Phone: (469) 948-2802",
+        "email": "boughtona@lisd.net",
+        "students": ""
+    },
+    "garlinger":{
+        "pictureUrl": "JacobGarlinger.jpg",
+        "video": "gyN_V9ss7fY",
+        "name": "Amy Boughton",
+        "title": "Associate Principal",
+        "phone": "Phone: (469) 948-3065",
+        "email": "garlingerj@lisd.net",
+        "students": "Students Lin - Ngo"
+    },
+    "zavar":{
+        "pictureUrl": "VanessaZavar.jpg",
+        "video": "SDPd6ufTRHM",
+        "name": "Dr. Vanessa Zavar",
+        "title": "Assistant Principal",
+        "phone": "Phone: (469) 948-2808",
+        "email": "zavarv@lisd.net",
+        "students": "Students A-Coli"
+    },
+    "croll":{
+        "pictureUrl": "download.png",
+        "video": "c3Ys663pTnI",
+        "name": "Glen Croll",
+        "title": "Assistant Principal",
+        "phone": "Phone: (469) 948-2818",
+        "email": "crollg@lisd.net",
+        "students": "Students Coll-Haw"
+    },
+    "mccoo":{
+        "pictureUrl": "BrianMccoo.jpg",
+        "video": "c3Ys663pTnI",
+        "name": "Brian McCoo",
+        "title": "Assistant Principal",
+        "phone": "Phone: (469) 948-2814",
+        "email": "mccoob@lisd.net",
+        "students": "Students Ngu - Sc"
+    },
+    "kushnir":{
+        "pictureUrl": "JackieKushnir.jpg",
+        "video": "c3Ys663pTnI",
+        "name": "Jacquelyn Kushnir",
+        "title": "Assistant Principal",
+        "phone": "Phone: (469) 948-2813",
+        "email": "kushnirj@lisd.net",
+        "students": "Students Hay - Lim"
+    },
+    "cragin":{
+        "pictureUrl": "LindaCragin.jpg",
+        "video": "c3Ys663pTnI",
+        "name": "Linda Cragin",
+        "title": "Assistant Principal",
+        "phone": "Phone: (469) 948-2804",
+        "email": "craginl@lisd.net",
+        "students": "Students SE - Z"
+    },
+}
--- school-web/profile.js
+++ school-web/profile.js
@@ -1,10 +1,26 @@
-var videoID;
+
 
 $(document).click(function (event) {
     var clicked = $(event.target.parentElement)[0].id;
     playVideo(clicked);
-    videoID = clicked + "-video"
 })
+
+
+
+function makePlayer(videoID, width, height) {
+    var player = new YT.Player(videoID + "-video", {
+        height: height,
+        width: width,
+        videoId: staff[videoID].video,
+        playerVars: {
+            'autoplay': 1,
+            'fs': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    })
+}
 
 function playVideo(adminID) {
     var adminVideo = document.getElementById(adminID + "-video");
@@ -17,122 +33,16 @@ function playVideo(adminID) {
     var screenWidth = window.innerWidth;
     var playerWidth;
     var playerHeight;
-    if (screenWidth < 400) {
-        playerWidth = '350';
-        playerHeight = '200';
+    if (screenWidth < 600) {
+        playerWidth = screenWidth - 40;
+        playerHeight = playerWidth / 1.64;
     } else {
         playerWidth = '640';
         playerHeight = '390';
     }
 
-    if (adminID == "boughton") {
-        var player1 = new YT.Player("boughton-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: 'c3Ys663pTnI',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-            }
-        });
-    }
 
-    if (adminID == "garlinger") {
-        var player2 = new YT.Player("garlinger-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: 'gyN_V9ss7fY',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-            }
-        });
-    }
-
-    if (adminID == "zavar") {
-        var player3 = new YT.Player("zavar-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: 'SDPd6ufTRHM',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-            }
-        });
-    }
-
-    if (adminID == "croll") {
-        var player4 = new YT.Player("croll-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: 'F08youaPV8U',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-
-            }
-        });
-    }
-
-    if (adminID == "mccoo") {
-        var player5 = new YT.Player("mccoo-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: 'AOLg5IzsSnk',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-
-            }
-        });
-    }
-
-    if (adminID == "kushnir") {
-        var player6 = new YT.Player("kushnir-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: '8hGm0LP8qbc',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-
-            }
-        });
-    }
-
-    if (adminID == "cragin") {
-        var player7 = new YT.Player("cragin-video", {
-            height: playerHeight,
-            width: playerWidth,
-            videoId: '4xl3JSXg7UY',
-            playerVars: {
-                'autoplay': 1,
-                'fs': 1
-            },
-            events: {
-                'onReady': onPlayerReady,
-
-            }
-        });
-    }
+    makePlayer(adminID,playerWidth,playerHeight);
 
 }
 
