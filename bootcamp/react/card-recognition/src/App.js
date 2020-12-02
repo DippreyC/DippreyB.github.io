7d3b0998d66e2f459b099d5acff33b75b143d3c6
DippreyB <48030605+DippreyB@users.noreply.github.com>
2020-12-02 13:09:48 -0600

    first 60 cards in model

--- bootcamp/react/card-recognition/src/App.js
+++ bootcamp/react/card-recognition/src/App.js
@@ -2,48 +2,56 @@ import * as ml5 from "ml5";
 import React, { useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import './App.css';
-import queen from "./img/queen5.jpg";
+import queen from "./img/aust1.jpg";
 
 
 const App = () => {
 
   
   const [scannedCardName,setScannedCardName] = React.useState("Scanning...");
-  const [detectedText, setDetectedText] = React.useState("Detecting Text");
   const webcamRef = useRef(null);
  
  const runApp = async () => {
-  let classifier =  ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/m68awb2T0/model.json',modelLoaded);
-  setInterval( () => {
-    detect(classifier);
-  }, 100
-  )
+  let classifier =  ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/6_biR2NFv/model.json', ()=>{console.log("Model Loaded")});
+  if(!webcamRef.current.props.muted){
+    setInterval( () => {
+      detect(classifier);
+    }, 100
+    )
+  }
+  else{
+    classifier.classify(document.querySelector("#img"), (err,results) =>{
+      console.log(results);
+      let cardLabel = results[0].label;
+      let percentConfidence = results[0].confidence.toFixed(3)*100;
+      if(percentConfidence > 30)
+        setScannedCardName(cardLabel+" "+percentConfidence+"%");
+      else
+        setScannedCardName("Can't read card...");
+    });
+  }
 }
 
  const detect = async (classifier) => {
     classifier.classify(document.querySelector("#webcam"), (err, results) =>{
       console.log(results);
-      //if(!err) setScannedCardName(getHighestConfidence(results).label);
+      let cardLabel = results[0].label;
+      let percentConfidence = results[0].confidence.toFixed(3)*100;
+      if(percentConfidence > 30)
+        setScannedCardName(cardLabel+" "+percentConfidence+"%");
+      else
+        setScannedCardName("Can't read card...");
   })
  }
 
   
 
-  const modelLoaded = () => {
-    console.log("model Loaded");
-  
-  }
-
-  
-  
-  
   useEffect( () => {
    runApp();
-
   })   
 
   return (
-    <>
+    <div id="webcam-container">
     <img src={queen} style={{width: "300px"}} alt="Queen Marchesa Pic" id="img"/>
     <div>{scannedCardName}</div>
     <Webcam
@@ -51,29 +59,16 @@ const App = () => {
           ref={webcamRef}
           muted={true} 
           style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
             textAlign: "center",
             zindex: 9,
             width: 640,
             height: 480,
           }}
         />
-    </>
+    </div>
   );
 }
 
-const getHighestConfidence = (results) => {
-  let highest = {label: "", confidence: 0};
-  results.map( (result) => {
-    if(result.confidence > highest.confidence)
-      highest = result;
-  })
 
-  return highest;
-}
 
 export default App;
